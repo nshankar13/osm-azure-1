9e567f922f7272334ab36bfc58035a38bcc4284b
nshankar13 <nshankar@microsoft.com>
2021-02-09 12:55:40 -0500

    Add e2e for upgrade

--- scripts/update-extension.sh
+++ scripts/update-extension.sh
@@ -4,21 +4,31 @@ source .env
 
 RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-arc-osm-system}"
 EXTENSION_NAME="${EXTENSION_NAME:-osm}"
-API_VERSION="${API_VERSION:-2020-07-01-preview}"
+EXTENSION_TYPE="${EXTENSION_TYPE:-Microsoft.openservicemesh}"
 EXTENSION_SETTINGS=$1
+K8S_EXTENSION_VERSION="${K8S_EXTENSION_VERSION:-0.1PP.12}"
 
+<<<<<<< HEAD
 export RESOURCEID="subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME"
 
 echo "Azure Resource ID: $RESOURCEID"
+=======
+az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
+
+az extension remove --name k8s-extension
+
+git clone https://github.com/Azure/azure-arc-kubernetes-preview
+
+az extension add --source ./extensions/k8s_extension-$K8S_EXTENSION_VERSION-py2.py3-none-any.whl --yes
+>>>>>>> Add e2e for upgrade
 
 if [[ -z "$EXTENSION_SETTINGS" ]]; then
-    EXTENSION_SETTINGS="osm_extension.json"
-    jq -n \
-        --arg tag "$CHECKOUT_TAG" \
-        --arg namespace "$RELEASE_NAMESPACE" \
-        '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } } } }' > osm_extension.json
+    az k8s-extension create --cluster-name $CLUSTERNAME --resource-group $RESOURCEGROUP --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --release-train staging --name $EXTENSION_NAME --release-namespace arc-osm-system --version $CHECKOUT_TAG
+else 
+    az k8s-extension create --cluster-name $CLUSTERNAME --resource-group $RESOURCEGROUP --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --release-train staging --name $EXTENSION_NAME --release-namespace arc-osm-system --version $CHECKOUT_TAG --configuration-protected-settings-file $EXTENSION_SETTINGS
 fi
 
+<<<<<<< HEAD
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
 # enable the OSM Extension
@@ -27,3 +37,5 @@ az rest \
    --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" \
    --body @$EXTENSION_SETTINGS \
    --debug
+=======
+>>>>>>> Add e2e for upgrade
