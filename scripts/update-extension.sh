03865facbfe72bdfa82794d21da73de6f5597857
nshankar13 <nshankar@microsoft.com>
2021-02-09 14:58:18 -0500

    Removed k8s installation from update-extension.sh

--- scripts/update-extension.sh
+++ scripts/update-extension.sh
@@ -8,19 +8,9 @@ EXTENSION_TYPE="${EXTENSION_TYPE:-Microsoft.openservicemesh}"
 EXTENSION_SETTINGS=$1
 K8S_EXTENSION_VERSION="${K8S_EXTENSION_VERSION:-0.1PP.12}"
 
-<<<<<<< HEAD
 export RESOURCEID="subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME"
 
 echo "Azure Resource ID: $RESOURCEID"
-=======
-az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
-
-az extension remove --name k8s-extension
-
-git clone https://github.com/Azure/azure-arc-kubernetes-preview
-
-az extension add --source ./extensions/k8s_extension-$K8S_EXTENSION_VERSION-py2.py3-none-any.whl --yes
->>>>>>> Add e2e for upgrade
 
 if [[ -z "$EXTENSION_SETTINGS" ]]; then
     az k8s-extension create --cluster-name $CLUSTERNAME --resource-group $RESOURCEGROUP --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --release-train staging --name $EXTENSION_NAME --release-namespace arc-osm-system --version $CHECKOUT_TAG
@@ -28,7 +18,6 @@ else
     az k8s-extension create --cluster-name $CLUSTERNAME --resource-group $RESOURCEGROUP --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --release-train staging --name $EXTENSION_NAME --release-namespace arc-osm-system --version $CHECKOUT_TAG --configuration-protected-settings-file $EXTENSION_SETTINGS
 fi
 
-<<<<<<< HEAD
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
 # enable the OSM Extension
@@ -37,5 +26,3 @@ az rest \
    --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" \
    --body @$EXTENSION_SETTINGS \
    --debug
-=======
->>>>>>> Add e2e for upgrade
